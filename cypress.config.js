63b348bd1bf9a364984f686129dd47815ca0a732
Speraltaa10 <santi.peralta10@gmail.com>
2024-12-21 13:35:37 -0500

    Versión final

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,25 @@
-const { lighthouse, prepareAudit } = require('cypress-audit');
+const { lighthouse, prepareAudit } = require("@cypress-audit/lighthouse");
 
 module.exports = {
   e2e: {
-    baseUrl: 'https://www.amazon.com/',
-    "chromeWebSecurity": false,
+    baseUrl: "https://www.amazon.com", // baseUrl correcto
     setupNodeEvents(on, config) {
+      const { lighthouse, prepareAudit } = require("@cypress-audit/lighthouse");
+
       on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.name === 'chrome') {
+          launchOptions.args.push('--disable-gpu');
+          launchOptions.args.push('--no-sandbox'); // Ayuda en entornos CI
+          launchOptions.args.push('--disable-dev-shm-usage');
+        }
         prepareAudit(launchOptions);
         return launchOptions;
       });
 
-      on('task', {
-        lighthouse: lighthouse(),
+      on("task", {
+        lighthouse: lighthouse(), // Define la tarea de Lighthouse
       });
     },
+    chromeWebSecurity: false,
   },
 };
